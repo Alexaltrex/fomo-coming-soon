af45614f6dbe49f831e12be0ea2fb7ddb7e80f72
AlexAltrex <alexaltrex@mail.ru>
2024-07-24 13:59:03 +0300

    fix

--- src/components/P0_EnterCode/EnterCode.tsx
+++ src/components/P0_EnterCode/EnterCode.tsx
@@ -2,13 +2,18 @@ import style from "./EnterCode.module.scss";
 import {svgIcons} from "../../assets/svgIcons";
 import {InviteCodeForm} from "./InviteCodeForm/InviteCodeForm";
 import {ConnectWallet} from "./ConnectWallet/ConnectWallet";
-import {useState} from "react";
+import React, {useState} from "react";
 import {SocialLinks} from "../_common/SocialLinks/SocialLinks";
 import {useNavigate} from "react-router-dom";
+// @ts-ignore
+import {FlipItem} from "../_common/FlipItem";
 
 export const EnterCode = () => {
     const [connectWallet, setConnectWallet] = useState(false)
-const navigate = useNavigate();
+    const navigate = useNavigate();
+
+    //const [count, setCount] = useState(1);
+
 
     return (
         <div className={style.enterCode}>
@@ -18,6 +23,15 @@ const navigate = useNavigate();
             </div>
 
             <InviteCodeForm onClick={() => setConnectWallet(true)}/>
+
+            {/*<div className={style.tickWrapper}*/}
+            {/*>*/}
+            {/*    <FlipItem value={count}/>*/}
+            {/*</div>*/}
+
+            {/*<button onClick={() => setCount(count + 1)}>*/}
+            {/*    add*/}
+            {/*</button>*/}
 
             <SocialLinks/>
 
